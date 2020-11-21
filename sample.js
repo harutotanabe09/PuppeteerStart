7933fee9434b1470419a5fca89358a895a67266f
田邊 悠人 <haruto_tanabe@en-japan.com>
2020-11-21 22:09:23 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true,  // ブラウザが動く様子を確認する
+        slowMo: 300  // 動作確認しやすいようにpuppeteerの操作を遅延させる
+    })
+    const page = await browser.newPage()
+
+    //chromeを開く
+    await page.goto('https://www.google.com/')
+    // 検索窓に「こんにちは」と入力
+    await page.type('input[name=q]', 'こんにちは', { delay: 50 }) 
+    //検索
+    await page.click('input[type="submit"]')
+    //スクリーンショット
+    await page.screenshot({ path: 'screenshot/sample1.png' })
+
+    await browser.close()
+})()
+
