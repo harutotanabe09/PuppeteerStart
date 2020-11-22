ab9ee4d4b249030fc796bb3692f7fa71cb0e5ddd
harutotanabe09@gmail.com <harutotanabe09@gmail.com>
2020-11-22 20:28:23 +0900

    serveless add lamdba

--- src/models/lambda/handler.js
+++ src/models/lambda/handler.js
@@ -1,14 +1,19 @@
-'use strict';
+const puppeteer = require('puppeteer');
+const { getChrome } = require('./chrome-script');
 
-module.exports.app = async event => {
+module.exports.app = async (event) => {
+    const { url } = event.queryStringParameters;
+    const chrome = await getChrome();
+    const browser = await puppeteer.connect({
+        browserWSEndpoint: chrome.endpoint,
+    });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const content = await page.evaluate(() => document.body.innerHTML);
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Go Serverless v1.0! Your function executed successfully!!!!',
-                input: event,
-            },
-            null
-        ),
+        body: JSON.stringify({
+            content,
+        }),
     };
 };
