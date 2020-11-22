d4f3f94edeb43c7420b55ea0e5e654e09b89baa7
harutotanabe09@gmail.com <harutotanabe09@gmail.com>
2020-11-22 13:54:14 +0900

    test

--- sample.js
+++ sample.js
@@ -1,12 +1,11 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+(async ()    => {
     const browser = await puppeteer.launch({
         headless: false,  // ブラウザが動く様子を確認する
         slowMo: 300  // 動作確認しやすいようにpuppeteerの操作を遅延させる
     })
     const page = await browser.newPage()
-
     //chromeを開く
     await page.goto('https://www.google.com/')
     // 検索窓に「こんにちは」と入力
