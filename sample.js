53886f1e6becae683f8e1919b70648f5d4bdb3cd
田邊 悠人 <haruto_tanabe@en-japan.com>
2020-11-22 14:09:14 +0900

    refactor: 💡 eslint add

--- sample.js
+++ sample.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({
         headless: false,  // ブラウザが動く様子を確認する
         slowMo: 300  // 動作確認しやすいようにpuppeteerの操作を遅延させる
-    })
-    const page = await browser.newPage()
+    });
+    const page = await browser.newPage();
     //chromeを開く
     await page.goto('https://www.google.com/')
     // 検索窓に「こんにちは」と入力
