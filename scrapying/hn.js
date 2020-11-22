536a18da04c8998b4af5775b6071c8368b326c74
harutotanabe09@gmail.com <harutotanabe09@gmail.com>
2020-11-22 17:56:59 +0900

    list arrange

---
+++ scrapying/hn.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+
+(async function () {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://news.ycombinator.com', { waitUntil: 'networkidle2' });
+    await page.pdf({ path: 'hn.pdf', format: 'A4' });
+    await browser.close();
+    document.body.textContent = "Hello, " + name + "!";
+})();
+
