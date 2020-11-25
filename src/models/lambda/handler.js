7f1bdcc8465763b937ae6dd0b13bf71cc4ecc109
harutotanabe09@gmail.com <harutotanabe09@gmail.com>
2020-11-25 23:53:01 +0900

    :triangular_flag_on_post: add data

--- src/models/lambda/handler.js
+++ src/models/lambda/handler.js
@@ -1,25 +1,17 @@
 const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
 
-exports.handler = async (event, context) => {
-    let result = null;
-    let browser = null;
-    try {
-        browser = await puppeteer.launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
-        });
-        let page = await browser.newPage();
-        await page.goto(event.url || 'https://yahoo.co.jp/');
-        result = await page.title();
-    } catch (error) {
-        return context.fail(error);
-    } finally {
-        if (browser !== null) {
-            await browser.close();
-        }
-    }
-    return context.succeed(result);
+module.exports.app = async(event, context) => {
+    console.log(event);
+    console.log(context);
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+    let page = await browser.newPage();
+    await page.goto('https://dev.classmethod.jp/');
+    const result = await page.title();
+    await browser.close();
+    return result;
 };
